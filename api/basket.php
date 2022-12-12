41d675412ee87cb59559ec6f47c29452b001bbae
Rapp Anna <anjarapp18@gmail.com>
2022-12-12 16:59:06 +0700

    Исправлены запросы basket Коды ошибок, вынесен checkAuthorize, исправлена ошибка с order_id

--- api/basket.php
+++ api/basket.php
@@ -1,26 +1,23 @@
 <?php
     require_once "modules/headers.php";
     require_once "modules/jwt.php";
+    require_once "modules/check_authorize.php";
 
     function route($method, $urlList, $requestData) {
         global $link;
-        $token = substr(getallheaders()['Authorization'], 7);
-        $emailFromToken = getPayload($token)['email'];
-        $user = $link->query("SELECT user_id FROM user WHERE email='$emailFromToken'")->fetch_assoc();
-        $userID = $user['user_id'];
-        $checkToken = $link->query("SELECT token_id FROM token WHERE token='$token'")->fetch_assoc();
-        if ($checkToken) {
+        $userID = checkAuthorize();
+        if (!$userID) {
             setHTTPStatus('401','Token not specified or not valid');
             return;
         }
         switch ($method) {
             case "GET":
                 if (count($urlList) != 2) {
-                    setHTTPStatus('403',"Method '$method' not allowed");
+                    setHTTPStatus('405',"Method '$method' not allowed");
                     break;
                 }
                 $basketList = $link->query("SELECT dish.dish_id, name, price, amount, image FROM dish 
-                                                    JOIN dish_basket ON dish_basket.dish_id = dish.dish_id 
+                                                    JOIN dish_basket ON dish_basket.dish_id = dish.dish_id AND dish_basket.order_id is null
                                                     JOIN user ON user.user_id = '$userID'");
                 $result = [];
                 foreach ($basketList as $dish) {
@@ -37,35 +34,45 @@
             break;
             case "POST":
                 if (count($urlList) != 4 || $urlList[2] != 'dish') {
-                    setHTTPStatus('403',"Method '$method' not allowed");
+                    setHTTPStatus('405',"Method '$method' not allowed");
                     break;
                 }
                 $checkDish = $link->query("SELECT dish_id FROM dish WHERE dish_id='$urlList[3]'")->fetch_assoc();
                 if (!$checkDish) {
                     setHTTPStatus('404', "Dish with id = '$urlList[3]' not found");
                 }
-                $checkDishInBasket = $link->query("SELECT amount FROM dish_basket WHERE user_id='$userID' AND dish_id='$urlList[3]'")->fetch_assoc();
+                $checkDishInBasket = $link->query("SELECT amount FROM dish_basket WHERE user_id='$userID' AND dish_id='$urlList[3]' AND order_id is null")->fetch_assoc();
                 if ($checkDishInBasket) {
-                    $updateDishInBasket = $link->query("UPDATE dish_basket SET amount=amount+1 WHERE user_id='$userID' AND dish_id='$urlList[3]'");
+                    $updateDishInBasket = $link->query("UPDATE dish_basket SET amount=amount+1 WHERE user_id='$userID' AND dish_id='$urlList[3]' AND order_id is null");
                 } else {
                     $addDish = $link->query("INSERT INTO dish_basket(user_id, dish_id, amount)
                                                                 VALUES('$userID', '$urlList[3]', 1)");
                 }
             break;
             case "DELETE":
-                if (count($urlList) != 4 || $urlList[2] != 'dish' || !$_GET['increase']) {
-                    setHTTPStatus('403',"Method '$method' not allowed");
+                if (count($urlList) != 4 || $urlList[2] != 'dish') {
+                    setHTTPStatus('405',"Method '$method' not allowed");
+                    break;
+                }
+                if (!$_GET['increase']) {
+                    setHTTPStatus('400',"Value 'Increase' not specified");
                     break;
                 }
                 $checkDish = $link->query("SELECT dish_id FROM dish WHERE dish_id='$urlList[3]'")->fetch_assoc();
                 if (!$checkDish) {
                     setHTTPStatus('404', "Dish with id = '$urlList[3]' not found");
+                    break;
+                }
+                $checkDishInBasket = $link->query("SELECT amount FROM dish_basket WHERE dish_id='$urlList[3]' AND order_id is null")->fetch_assoc();
+                if (!$checkDishInBasket) {
+                    setHTTPStatus('404', "Dish with id = '$urlList[3]' not found in a basket");
+                    break;
                 }
                 $increaseBoolVal = filter_var($_GET['increase'], FILTER_VALIDATE_BOOLEAN);
-                if ($increaseBoolVal) {
-                    $decreaseDishInBasket = $link->query("UPDATE dish_basket SET amount=amount-1 WHERE user_id='$userID' AND dish_id='$urlList[3]'");
+                if ($increaseBoolVal && $checkDishInBasket['amount']-1 > 0) {
+                    $decreaseDishInBasket = $link->query("UPDATE dish_basket SET amount=amount-1 WHERE user_id='$userID' AND dish_id='$urlList[3]' AND order_id is null");
                 } else {
-                    $deleteDishFromBasket = $link->query("DELETE FROM dish_basket WHERE user_id='$userID' AND dish_id='$urlList[3]'");
+                    $deleteDishFromBasket = $link->query("DELETE FROM dish_basket WHERE user_id='$userID' AND dish_id='$urlList[3]' AND order_id is null");
                 }
             break;
         }
---
+++ modules/check_authorize.php
@@ -0,0 +1,19 @@
+<?php
+
+    require_once "modules/jwt.php";
+
+function checkAuthorize() {
+    global $link;
+    $token = substr(getallheaders()['Authorization'], 7);
+    if (!isValid($token) || isExpired($token)) {
+        return null;
+    }
+    $emailFromToken = getPayload($token)['email'];
+    $user = $link->query("SELECT user_id FROM user WHERE email='$emailFromToken'")->fetch_assoc();
+    $userID = $user['user_id'];
+    $checkToken = $link->query("SELECT token_id FROM token WHERE token='$token'")->fetch_assoc();
+    if (!$checkToken) {
+        return $userID;
+    }
+    return null;
+}
