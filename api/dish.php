02780f2f917d2ae3b34ed986cf18781264f3490b
Rapp Anna <anjarapp18@gmail.com>
2022-12-14 00:58:11 +0700

    Доделала запросы в dish

--- api/dish.php
+++ api/dish.php
@@ -1,25 +1,33 @@
 <?php
     require_once "modules/headers.php";
     require_once "modules/jwt.php";
+    require_once "modules/validateGetParam.php";
+    require_once "modules/check_authorize.php";
+
     function route($method, $urlList, $requestData) {
         global $link;
         switch (count($urlList)) {
             case 2:
+                if ($method != "GET") {
+                    setHTTPStatus('405',"Method '$method' not allowed");
+                    return;
+                }
                 $url = strstr($_SERVER['REQUEST_URI'], '?');
                 $url = substr($url, 1);
                 $url = str_replace("categories", "categories[]", $url);
                 parse_str($url, $get);
 
-                $pageInfo = array('size' => 6);
-                $pageNumber = $get['page'];
-                if (!$pageNumber) {
-                    $pageNumber = 1;
-                } else if ($pageNumber <= 0) {
-                    setHTTPStatus('404', "Page '$pageNumber' does not exist");
+                if (!validateGetParam($get)) {
                     return;
                 }
-                $numberOfRecords = $pageInfo['size'];
-                $from = $pageInfo['size']*($pageInfo['currentPage'] - 1);
+
+                $pageInfo = ['size' => 6];
+                $pageNumber = $get['page'];
+                if ($pageNumber == '') {
+                    $pageNumber = 1;
+                }
+                $pageSize = $pageInfo['size'];
+                $from = $pageSize*($pageNumber - 1);
 
                 $categoriesList = [];
                 if ($get['categories'] == '') {
@@ -31,38 +39,49 @@
                 }
                 $categories = '\'' . implode('\', \'', $categoriesList) . '\'';
 
-                if ($get['vegetarian'] == '') {
+                if ($get['vegetarian'] == '' || $get['vegetarian'] == 0) {
                     $vegetarian = '1, 0';
                 } else {
-                    $vegetarian = boolval($get['vegetarian']);
+                    $vegetarian = 1;
+                }
+                if ($get['sorting']) {
+                    $sorting = ltrim(preg_replace( '/[A-Z]/', ' $0', $get['sorting']));
+                    $sorting = mb_strtolower($sorting);
+                    $sorting = ltrim(preg_replace('/asc/', 'ASC', $sorting));
+                    $sorting = ltrim(preg_replace('/desc/', 'DESC', $sorting));
+                } else {
+                    $sorting = 'dish_id ASC';
                 }
 
-                $sorting = ltrim(preg_replace( '/[A-Z]/', ' $0', $get['sorting']));
-                $sorting = mb_strtolower($sorting);
-                $sorting = ltrim(preg_replace('/asc/', 'ASC', $sorting));
-                $sorting = ltrim(preg_replace('/desc/', 'DESC', $sorting));
-
-                $listDishes = $link->query("SELECT * FROM dish WHERE (category IN ($categories) AND vegetarian IN ($vegetarian)) ORDER BY $sorting");
-
+                $listDishes = $link->query("SELECT * FROM dish WHERE (category IN ($categories) AND vegetarian IN ($vegetarian)) ORDER BY $sorting LIMIT $pageSize OFFSET $from");
                 foreach ($listDishes as $dish) {
                     $result['dishes'][] = $dish;
                 }
-                $countPages = $link->query("SELECT COUNT(*) AS countPages FROM dish WHERE (category IN ($categories) AND vegetarian IN ($vegetarian)) ORDER BY $sorting")->fetch_assoc();
-                $pageInfo['count'] = intval($countPages['countPages']);
+
+                $countPages = $link->query("SELECT COUNT(*) AS countPages FROM dish WHERE (category IN ($categories) AND vegetarian IN ($vegetarian))")->fetch_assoc();
+                $pageInfo['count'] = ceil($countPages['countPages'] / $pageSize);
                 $pageInfo['current'] = $pageNumber;
                 $result['pagination'] = $pageInfo;
+                if ($get['page'] > $pageInfo['count']) {
+                    $messageResult = [
+                        'status' => 'Error',
+                        'message' => []
+                    ];
+                    $messageResult['message'] = ["page" => "Invalid value for attribute page"];
+                    setHTTPStatus('400', $messageResult);
+                    return;
+                }
                 echo json_encode($result);
                 break;
-
             case 3:
                 if ($method != "GET") {
-                    setHTTPStatus('403',"Method '$method' not allowed");
-                    break;
+                    setHTTPStatus('405',"Method '$method' not allowed");
+                    return;
                 }
                 $checkDish = $link->query("SELECT dish_id FROM dish WHERE dish_id='$urlList[2]'")->fetch_assoc();
                 if (!$checkDish) {
-                    setHTTPStatus('404', "Dish with id = '$urlList[3]' not found");
-                    break;
+                    setHTTPStatus('404', "Dish with id = '$urlList[2]' not found");
+                    return;
                 }
                 $dishInfo = $link->query("SELECT * FROM dish WHERE dish_id='$urlList[2]'")->fetch_assoc();
                 $dishRating = $link->query("SELECT * FROM dish_rating WHERE dish_id='$urlList[2]'");
@@ -79,29 +98,29 @@
                     'price' => $dishInfo['price'],
                     'image' => $dishInfo['image'],
                     'vegetarian' => boolval($dishInfo['vegetarian']),
-                    'rating' => $sumRating/$countRating,
+                    'rating' => $countRating == 0? 0: $sumRating/$countRating,
                     'category' => $dishInfo['category']
                 ];
                 echo json_encode($result);
                 break;
             case 5:
-                if ($method != "GET" || $urlList[3]!="rating" || $urlList[4]!="check") {
-                    setHTTPStatus('403',"Method '$method' not allowed");
-                    break;
+                if ($method != "GET") {
+                    setHTTPStatus('405',"Method '$method' not allowed");
+                    return;
                 }
-                $token = substr(getallheaders()['Authorization'], 7);
-                $emailFromToken = getPayload($token)['email'];
-                $user = $link->query("SELECT user_id FROM user WHERE email='$emailFromToken'")->fetch_assoc();
-                $userID = $user['user_id'];
-                $checkToken = $link->query("SELECT token_id FROM token WHERE token='$token'")->fetch_assoc();
-                if ($checkToken) {
+                if ($urlList[3] != "rating" || $urlList[4] != "check") {
+                    setHTTPStatus('404','Missing resource is requested');
+                    return;
+                }
+                $userID = checkAuthorize();
+                if (!$userID) {
                     setHTTPStatus('401','Token not specified or not valid');
                     return;
                 }
                 $checkDish = $link->query("SELECT dish_id FROM dish WHERE dish_id='$urlList[2]'")->fetch_assoc();
                 if (!$checkDish) {
-                    setHTTPStatus('404', "Dish with id = '$urlList[3]' not found");
-                    break;
+                    setHTTPStatus('404', "Dish with id = '$urlList[2]' not found");
+                    return;
                 }
                 $dishRatingCheck = $link->query("SELECT * FROM dish_rating WHERE dish_id = '$urlList[2]' AND user_id='$userID'")->fetch_assoc();
                 if ($dishRatingCheck){
@@ -111,39 +130,46 @@
                 }
                 break;
             case 4:
-                if ($method != "POST" || $urlList[3]!="rating") {
-                    setHTTPStatus('403',"Method '$method' not allowed");
-                    break;
+                if ($method != "POST") {
+                    setHTTPStatus('405',"Method '$method' not allowed");
+                    return;
                 }
-                $token = substr(getallheaders()['Authorization'], 7);
-                $emailFromToken = getPayload($token)['email'];
-                $user = $link->query("SELECT user_id FROM user WHERE email='$emailFromToken'")->fetch_assoc();
-                $userID = $user['user_id'];
-                $checkToken = $link->query("SELECT token_id FROM token WHERE token='$token'")->fetch_assoc();
-                if ($checkToken) {
+                if ($urlList[3]!="rating") {
+                    setHTTPStatus('404','Missing resource is requested');
+                    return;
+                }
+                $userID = checkAuthorize();
+                if (!$userID) {
                     setHTTPStatus('401','Token not specified or not valid');
                     return;
                 }
                 $checkDish = $link->query("SELECT dish_id FROM dish WHERE dish_id='$urlList[2]'")->fetch_assoc();
                 if (!$checkDish) {
-                    setHTTPStatus('404', "Dish with id = '$urlList[3]' not found");
-                    break;
+                    setHTTPStatus('404', "Dish with id = '$urlList[2]' not found");
+                    return;
+                }
+                $checkOrder = $link->query("SELECT order_id FROM dish_basket WHERE user_id='$userID' 
+                                                  AND dish_id='$urlList[2]' AND order_id is not null")->fetch_assoc();
+                if (!$checkOrder) {
+                    setHTTPStatus('403', "The dish was not ordered");
+                    return;
                 }
                 $ratingScore = $_GET['ratingScore'];
-                if ($ratingScore>10 || $ratingScore<0 || !$ratingScore) {
+                if ($ratingScore > 10 || $ratingScore < 0 || $ratingScore == '') {
                     setHTTPStatus('400', "Invalid value for rating");
-                    break;
+                    return;
                 }
-
-                //TODO сделать проверку на то, что user заказывал это блюдо (403)
 
                 $checkRating = $link->query("SELECT rating FROM dish_rating WHERE user_id='$userID' AND dish_id='$urlList[2]'")->fetch_assoc();
                 if (!$checkRating) {
                     $addRating = $link->query("INSERT INTO dish_rating(user_id, dish_id, rating)
                                                                 VALUES('$userID', '$urlList[2]', '$ratingScore')");
                 } else {
-                    $updateRating = $link->query("UPDATE dish_rating SET rating='$ratingScore' WHERE user_id='$userID AND dish_id='$urlList[2]");
+                    $updateRating = $link->query("UPDATE dish_rating SET rating='$ratingScore' WHERE user_id='$userID' AND dish_id='$urlList[2]'");
                 }
-
+                break;
+            default:
+                setHTTPStatus('404', 'Missing resource is requested');
+                break;
         }
     }
